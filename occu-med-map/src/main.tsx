357f52aa019b2c9dce475b9461758626fc940079
Occumed79 <Alex.Ayvazian@logan.edu>
2026-06-29 01:38:20 -0700

    fix: load final map styling

--- occu-med-map/src/main.tsx
+++ occu-med-map/src/main.tsx
@@ -3,5 +3,6 @@ import App from "./App";
 import "./index.css";
 import "./professional-overrides.css";
 import "./professional-hardening.css";
+import "./tahoe-liquid-glass.css";
 
 createRoot(document.getElementById("root")!).render(<App />);

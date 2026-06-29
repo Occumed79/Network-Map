4711587d9847a6089a17605f0b54ff0b9b663331
Occumed79 <Alex.Ayvazian@logan.edu>
2026-06-29 12:01:47 -0700

    fix: load light panel override

--- occu-med-map/src/main.tsx
+++ occu-med-map/src/main.tsx
@@ -5,5 +5,6 @@ import "./professional-overrides.css";
 import "./professional-hardening.css";
 import "./google-map-liquid-glass.css";
 import "./liquid-glass-final.css";
+import "./light-liquid-glass.css";
 
 createRoot(document.getElementById("root")!).render(<App />);

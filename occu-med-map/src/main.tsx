4c64789b3a08cd3c7176cb1582b8ced2e3441371
Occumed79 <Alex.Ayvazian@logan.edu>
2026-06-29 11:56:02 -0700

    fix: load final liquid glass panel styling

--- occu-med-map/src/main.tsx
+++ occu-med-map/src/main.tsx
@@ -4,5 +4,6 @@ import "./index.css";
 import "./professional-overrides.css";
 import "./professional-hardening.css";
 import "./google-map-liquid-glass.css";
+import "./liquid-glass-final.css";
 
 createRoot(document.getElementById("root")!).render(<App />);

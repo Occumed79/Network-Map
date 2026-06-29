ec978c0ccf83f87b5ae5b9f1845a57faa8702521
Occumed79 <Alex.Ayvazian@logan.edu>
2026-06-29 01:18:52 -0700

    fix: load hardened production map layout

--- occu-med-map/src/main.tsx
+++ occu-med-map/src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./professional-overrides.css";
+import "./professional-hardening.css";
 
 createRoot(document.getElementById("root")!).render(<App />);

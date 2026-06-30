616030acb01b62975e4abd4ff2d30a3d99841aaf
Occumed79 <Alex.Ayvazian@logan.edu>
2026-06-30 13:48:25 -0700

    feat: load Mapbox provider ETA ranking

--- occu-med-map/src/main.tsx
+++ occu-med-map/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./mapboxLeafletRuntime";
 import "./mapboxAdvancedControls";
+import "./mapboxProviderRanking";
 import App from "./App";
 import "./index.css";
 import "./professional-overrides.css";

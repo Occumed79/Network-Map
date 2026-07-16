082b723fb7a8d006a43e5142c11d6fc2a64419c6
Occumed79 <Alex.Ayvazian@logan.edu>
2026-07-16 03:00:57 -0700

    Run full NACCHO import and strict geocoder

--- api-server/src/index.ts
+++ api-server/src/index.ts
@@ -1,5 +1,7 @@
 import app from "./app";
 import { logger } from "./lib/logger";
+import { startNacchoLhdImport } from "./jobs/importNacchoLhd";
+import { startNacchoLhdGeocoder } from "./jobs/geocodeNacchoLhd";
 
 const rawPort = process.env["PORT"] ?? "3000";
 const port = Number(rawPort);
@@ -15,4 +17,6 @@ app.listen(port, (err) => {
   }
 
   logger.info({ port }, "Server listening");
+  startNacchoLhdImport();
+  startNacchoLhdGeocoder();
 });

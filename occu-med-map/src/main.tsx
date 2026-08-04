01b80d65232ec9ea96f51835327f5631919bbdde
Occumed79 <Alex.Ayvazian@logan.edu>
2026-08-04 16:29:38 -0700

    Load From and To route planner before map initialization

--- occu-med-map/src/main.tsx
+++ occu-med-map/src/main.tsx
@@ -5,6 +5,7 @@ import "./adminApiRuntime";
 import "./mapControlsBridgeRuntime";
 import "./completeProviderPinMirrorRuntime";
 import "./mapToolsCommandPanel";
+import "./routePlannerControlsRuntime";
 import "./dualMapEngineRuntime";
 import App from "./App";
 import "./dual-map-engines.css";

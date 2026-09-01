04a85fee168c41f7a5a3e7eec2c7d8cfc26d51da
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-01 00:14:27 -0700

    Expose Liechtenstein and San Marino stored registries

--- api-server/src/routes/storedInternationalRegistryLayers.ts
+++ api-server/src/routes/storedInternationalRegistryLayers.ts
@@ -25,6 +25,8 @@ const REGISTRIES: Record<string, StoredRegistryDefinition> = {
   "ukraine-nhsu": { sourceKey: "ua_nhsu_pmg", countryCode: "UA", countryName: "Ukraine" },
   "kosovo-moh-private": { sourceKey: "xk_moh_private_licensed", countryCode: "XK", countryName: "Kosovo" },
   "azerbaijan-tabib": { sourceKey: "az_tabib_healthcare", countryCode: "AZ", countryName: "Azerbaijan" },
+  "liechtenstein-lkv": { sourceKey: "li_lkv_okp", countryCode: "LI", countryName: "Liechtenstein" },
+  "san-marino-authorized": { sourceKey: "sm_authorized_healthcare", countryCode: "SM", countryName: "San Marino" },
 };
 
 function addParam(params: unknown[], value: unknown): string {

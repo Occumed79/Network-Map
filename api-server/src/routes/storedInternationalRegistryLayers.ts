edeabd6bde4e4810ee31329501dd1f4fffc34877
Occumed79 <Alex.Ayvazian@logan.edu>
2026-09-02 02:34:57 -0700

    Expose Armenia UHIF stored registry layer

--- api-server/src/routes/storedInternationalRegistryLayers.ts
+++ api-server/src/routes/storedInternationalRegistryLayers.ts
@@ -31,6 +31,7 @@ const REGISTRIES: Record<string, StoredRegistryDefinition> = {
   "north-macedonia-moh": { sourceKey: "mk_moh_health_centers", countryCode: "MK", countryName: "North Macedonia" },
   "denmark-sor-healthcare": { sourceKey: "dk_sor_healthcare", countryCode: "DK", countryName: "Denmark" },
   "andorra-cass": { sourceKey: "ad_cass_healthcare", countryCode: "AD", countryName: "Andorra" },
+  "armenia-uhif": { sourceKey: "am_uhif_healthcare", countryCode: "AM", countryName: "Armenia" },
   "bosnia-domestic": { sourceKey: "ba_domestic_healthcare", countryCode: "BA", countryName: "Bosnia & Herzegovina" },
 };
 
